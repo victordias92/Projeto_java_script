448bec426f688324686aaf29f6795ebe33f4a6b6
victor dias <victortov2010@gmail.com>
2024-10-06 02:00:29 -0300

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+console.log(4>7 || 6>9);
+console.log(!(!50>20 && 3>2));
+nome =(4 + 5)
+
+const people = [
+    {id:1, nome:"alice"},
+    {id:1, nome:"marcos"},
+    {id:1, nome:"marta"},
+    {id:1, nome:"julia"},
+
+];
+
+const names = people.map(person => person.nome);
+
+console.log(names);
+
